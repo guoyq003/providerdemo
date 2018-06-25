1df60fc5589bc00f2d2322af30a1ab660f933046
yiqing.guo <yiqing.guo@yiducloud.cn>
2018-06-25 15:00:51 +0800

    add

--- src/main/java/com/yidu/dubbo/provider/service/HelloService.java
+++ src/main/java/com/yidu/dubbo/provider/service/HelloService.java
@@ -2,6 +2,7 @@ package com.yidu.dubbo.provider.service;
 
 public interface HelloService {
     public String sayHello(String name,int age);
+    public String getUserInfo(String name,String password);
     public int getNum(int n);
     public double getsim(String str1, String str2);
     public void whileSleepFun();
--- src/main/java/com/yidu/dubbo/provider/service/impl/HelloServiceImpl.java
+++ src/main/java/com/yidu/dubbo/provider/service/impl/HelloServiceImpl.java
@@ -11,6 +11,17 @@ public class HelloServiceImpl implements HelloService {
     }
 
     @Override
+    public String getUserInfo(String name, String password) {
+        if (name.equals("test")&&"E10ADC3949BA59ABBE56E057F20F883E".equals(password)){
+            return "Hi,你的姓名是："+name+"年龄是20岁";
+        }else if ("test1".equals(name)&&password.equals("C8837B23FF8AAA8A2DDE915473CE0991")){
+            return "Hi,你的姓名是："+name+"年龄是32岁";
+        }else {
+            return "用户名或密码错误,请重新输入!";
+        }
+    }
+
+    @Override
     public int getNum(int n) {
         int num=0;
         for (int i=1;i<=n;i++){
